ce004c954709399b7b57a6dcc36f6e1c7cec8cc4
musoftware <MUGenius@windowslive.com>
2016-11-11 21:10:08 +0200

    CURL vs HttpRequest

---
+++ MultiThread/FastRequest.cs
@@ -0,0 +1,54 @@
+﻿using SeasideResearch.LibCurlNet;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThread
+{
+    class FastRequest
+    {
+        Easy easy = new Easy();
+
+        public static void GlobalInit()
+        {
+            Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+        }
+
+        public FastRequest(string url)
+        {
+          
+            Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+            easy.SetOpt(CURLoption.CURLOPT_URL, url);
+            easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+            easy.SetOpt(CURLoption.CURLOPT_SSL_VERIFYPEER, false);
+   
+        }
+
+        public Int32 OnWriteData(Byte[] buf, Int32 size, Int32 nmemb, Object extraData)
+        {
+            string data = System.Text.Encoding.UTF8.GetString(buf);
+            buffer += data;
+            return size * nmemb;
+        }
+        
+        private string buffer = string.Empty;
+
+        private bool callComplete = false;
+
+
+        public string Perform()
+        {
+            CURLcode data = easy.Perform();
+            easy.Dispose();
+            return buffer;
+        }
+
+        public static void GlobalCleanup()
+        {
+            Curl.GlobalCleanup();
+        }
+    }
+}
--- MultiThread/Program.cs
+++ MultiThread/Program.cs
@@ -1,13 +1,58 @@
-﻿using System;
+﻿using SeasideResearch.LibCurlNet;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
 using System.Text;
+using System.Threading;
 
 namespace MultiThread
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            ThreadPool.SetMinThreads(10, 5);
+            ThreadPool.SetMaxThreads(10, 5);
+            FastRequest.GlobalInit();
+            for (int i = 0; i < 1000; i++)
+            {
+                int tmp = i;
+                ThreadPool.QueueUserWorkItem((a) =>
+                {
+                    try
+                    {
+                        string access = "EAACEdEose0cBAP7CFo4cGOY3ZBjGZCT14D3M0wuFltzcMbZBkT1qz2EI3AHc2ysuSNZB8agSTXvIYnPfjng7FiNqYLhmseN5kP7owEac3CXsPtsWE56A3GAIVASxuhfHEFSKIaplqjegW9COu9Pksg8BbqZB1ZC4sZCD4hNHUZCQkAZDZD";
+
+                        Stopwatch s = new Stopwatch();
+                        s.Start();
+                        var res = new FastRequest("https://graph.facebook.com/me?access_token=" + access).Perform();
+                        s.Stop();
+                        long milli = s.ElapsedMilliseconds;
+                        s.Reset();
+                        s.Start();
+                        var res2 = new WebClient().DownloadString("https://graph.facebook.com/me?access_token=" + access);
+                        s.Stop();
+                        Console.WriteLine(tmp + " via .NET: "  + s.ElapsedMilliseconds + " via CURL: " + milli);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }, null);
+            }
+
+            Console.ReadKey();
+      
+            FastRequest.GlobalCleanup();
+
+        }
+        public static Int32 OnWriteData(Byte[] buf, Int32 size, Int32 nmemb, Object extraData)
+        {
+            string data = System.Text.Encoding.UTF8.GetString(buf);
+            Console.WriteLine(data);
+            return size * nmemb;
         }
     }
 }
