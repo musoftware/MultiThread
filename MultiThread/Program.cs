3bd975bce5fc6243adc90be7d8cb851d0b0a99a4
musoftware <MUGenius@windowslive.com>
2016-11-11 18:53:04 +0200

    Add project files.

---
+++ MultiThread/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
